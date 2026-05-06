1765b3e6f26ea8fe6c181136707f94abdf164436
sankalpdevtester <ohofufic0@gmail.com>
2026-05-07 02:00:04 +0530

    feat: add utility function to calculate investment returns

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,4 +1,3 @@
-```python
 from datetime import datetime
 from typing import Dict
 
@@ -9,19 +8,19 @@ def calculate_investment_returns(investment_data: Dict) -> float:
 
     Args:
     investment_data (Dict): A dictionary containing the initial amount, 
-                             current value, and time period of the investment.
+                            current value, and time period of the investment.
 
     Returns:
     float: The calculated return on investment.
     """
-    initial_amount = investment_data['initial_amount']
-    current_value = investment_data['current_value']
-    start_date = datetime.strptime(investment_data['start_date'], '%Y-%m-%d')
-    end_date = datetime.strptime(investment_data['end_date'], '%Y-%m-%d')
-    time_period = (end_date - start_date).days / 365  # calculate time period in years
+    initial_amount = investment_data.get('initial_amount', 0)
+    current_value = investment_data.get('current_value', 0)
+    start_date = datetime.strptime(investment_data.get('start_date', ''), '%Y-%m-%d')
+    end_date = datetime.strptime(investment_data.get('end_date', ''), '%Y-%m-%d')
+
+    time_period = (end_date - start_date).days / 365
 
     if time_period <= 0:
-        raise ValueError("Time period must be greater than zero")
+        return 0
 
     return ((current_value - initial_amount) / initial_amount) / time_period * 100
-```
