f17c9bfc2e33d07b178649337af05ee0f30dd9a4
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:50:46 +0530

    feat: add authentication utilities

--- src/utils/auth.py
+++ src/utils/auth.py
@@ -1,28 +1,31 @@
-from fastapi import Request
+from fastapi import HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from src.models.user import User
+from src.models import User
 from src.utils.helpers import get_db
-from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], default="bcrypt")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def authenticate_user(username: str, password: str, db: Session):
+def get_current_user(token: str, db = next(get_db())):
+    user = db.query(User).filter(User.username == token.split("_")[1]).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return user
+
+def authenticate_user(username: str, password: str, db = next(get_db())):
     user = db.query(User).filter(User.username == username).first()
-    if not user or not pwd_context.verify(password, user.password):
-        return False
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+    if user.password != password:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
     return user
 
-def get_current_user(request: Request, db: Session):
-    token = request.cookies.get("access_token")
-    if not token:
-        return None
-    user = db.query(User).filter(User.username == token).first()
-    if not user:
-        return None
-    return user
-
-def get_current_active_user(request: Request, db: Session):
-    user = get_current_user(request, db)
-    if not user:
-        return None
-    return user
+def requires_auth(func):
+    async def wrapper(*args, **kwargs):
+        token = kwargs.get("token")
+        if not token:
+            raise HTTPException(status_code=401, detail="Token is required")
+        user = get_current_user(token)
+        if not user:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return await func(*args, **kwargs)
+    return wrapper
